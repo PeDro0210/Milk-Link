f147712a24cedae856742e508c93ed8c4fa606be
PeDro0210 <pedroruben0210@gmail.com>
2025-02-28 11:42:22 -0600

    added env variables for backend

--- Milk-Backend/src/main.rs
+++ Milk-Backend/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use actix_cors::Cors;
+use actix_web::{App, HttpServer};
+
+use dotenv::dotenv;
+use std::env::var;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    //env variables
+    let host = var("HOST").unwrap();
+    let port = var("PORT").unwrap();
+
+    //needed casts
+    let port = port.parse::<u16>().unwrap();
+
+    //TODO:Wrap the cors and the services
+    HttpServer::new(|| App::new())
+        .bind((host, port))?
+        .run()
+        .await
 }
